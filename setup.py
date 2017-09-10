a30c78875436789d3270f7b8edb40f37e7d0d7e0
Andrew Stahlman <astahlman@lyft.com>
2017-09-10 16:02:01 -0700

    Set upper bound

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 version = '0.3.0'
 install_requires = [
-    'psutil>=3.4.0',
+    'psutil>=3.3.0,<=4.4.2',
 ]
 
 
